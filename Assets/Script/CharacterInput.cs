25951d74210b4185ca42b64dc06e73f3cec95201
badaso <vyalstn@hotmail.com>
2015-05-02 12:43:46 +0900

    프로토 시

---
+++ Assets/Script/CharacterInput.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterInput : MonoBehaviour {
+
+	void OnMouseDown(){
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+		
+		if (Physics.Raycast(ray, out hit)){
+			if (hit.collider.CompareTag("Enemy")){
+				print ("enemy click");	
+			}
+
+			if (hit.collider.CompareTag("Player")){
+				print ("player click");
+				
+			}
+		}
+		
+	}
+}
